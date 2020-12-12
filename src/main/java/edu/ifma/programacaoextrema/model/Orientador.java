e123f47e6ec7ace829492ba82e00806bed410274
Daiane Silva <daysmoraes@gmail.com>
2020-12-12 13:28:05 -0300

    D - 0.1.3 - atributo orientador

--- src/main/java/edu/ifma/programacaoextrema/model/Orientador.java
+++ src/main/java/edu/ifma/programacaoextrema/model/Orientador.java
@@ -1,4 +1,9 @@
 package edu.ifma.programacaoextrema.model;
 
 public class Orientador {
+    private String matricula;
+    private Long id;
+    private String nome;
+    private String cargo;
+    private String curso;
 }
